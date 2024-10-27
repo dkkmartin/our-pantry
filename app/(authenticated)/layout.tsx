bd5cac0a449907a67a0d9bdec00c7e164c066579
Martin Bruun <martinbruun1995@gmail.com>
2024-10-27 21:42:51 +0100

    feat: layouts

---
+++ app/(authenticated)/layout.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/header/header'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main className="p-4 min-h-[calc(100vh-4rem)] flex flex-col">{children}</main>
+    </>
+  )
+}

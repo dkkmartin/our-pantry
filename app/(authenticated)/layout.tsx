8e8f86d7884ca0f566808beb3b88b2d87fb441f6
Martin Bruun <martinbruun1995@gmail.com>
2024-10-27 23:19:52 +0100

    feat: add shadcn component & uuid

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -4,7 +4,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
-      <main className="p-4 min-h-[calc(100vh-4rem)] flex flex-col">{children}</main>
+      <main className="p-4 min-h-[calc(100vh-4rem)] flex flex-col">
+        {children}
+      </main>
     </>
   )
 }
